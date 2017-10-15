6162dec5c830846ad6a2d2caf11f2f4983f74787
muyingjie <1368468125@qq.com>
2017-10-15 22:02:10 +0800

    初始化文件

---
+++ web/src/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class App extends Component{
+    render() {
+        return (
+            <div>hello, react</div>
+        );
+    }
+}
+
+export default App;
+
+// module.exports = function() {
+//     var greet = document.createElement('div');
+//     greet.textContent = "Hi there and greetings!";
+//     return greet;
+// };
---
+++ web/webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    entry: __dirname + "/src/main.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "es2015", "react"
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
+    }
+};
