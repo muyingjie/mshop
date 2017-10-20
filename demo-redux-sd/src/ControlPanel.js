b289127053c4c845f78993892d3b4b328a43c6d2
zhaohuiziwo901 <CoderJerry@163.com>
2017-10-20 17:45:30 +0800

    初始化容器组件和傻瓜组件案例

---
+++ demo-redux-sd/src/ControlPanel.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class ControlPanel extends Component {
+    render() {
+        return (
+            <div>ControlPanel</div>
+        );
+    }
+}
+
+export default ControlPanel;
