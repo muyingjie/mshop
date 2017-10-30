57988162c0824e1968bc744cd08f501a8e4295a4
zhaohuiziwo901 <CoderJerry@163.com>
2017-10-30 17:42:47 +0800

    添加sass配置

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,3 +1,5 @@
+// extract-text-webpack-plugin，让webpack可以输出css格式的文件
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     devtool: 'eval-source-map',
     entry: __dirname + "/src/main.js",
@@ -27,9 +29,19 @@ module.exports = {
                         loader: "css-loader"
                     }
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
             }
         ]
     },
+    plugins: [
+        new ExtractTextPlugin("style.css"),
+    ],
     devServer: {
         contentBase: "./dist",
         port: 4000,
