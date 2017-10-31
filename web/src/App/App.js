141bf22c57f82518468b85001c1074a654cb4f7d
zhaohuiziwo901 <CoderJerry@163.com>
2017-10-31 16:59:43 +0800

    主体内容包裹对象清除浮动

--- web/src/App/App.js
+++ web/src/App/App.js
@@ -3,17 +3,22 @@ import LeftNav from "../LeftNav/LeftNav.js";
 
 import "./App.scss";
 
-const App = ({children}) => {
-    return (
-        <div className="wrap">
-            <div className="top-bar"></div>
-            <div>
-                <div className="nav-wrap fl">
-                    <LeftNav />
+class App extends Component {
+    constructor(prop) {
+        super(prop);
+    }
+    render() {
+        return (
+            <div className="wrap">
+                <div className="top-bar"></div>
+                <div className="clear">
+                    <div className="nav-wrap fl">
+                        <LeftNav />
+                    </div>
+                    <div className="main fl">{this.props.children}</div>
                 </div>
-                <div className="main fl">{children}</div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 export default App;
