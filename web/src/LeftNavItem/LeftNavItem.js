b1429468c0aa82af11b1bdce9ca332053f411327
zhaohuiziwo901 <CoderJerry@163.com>
2017-11-02 17:17:14 +0800

    更换动画库

--- web/src/LeftNavItem/LeftNavItem.js
+++ web/src/LeftNavItem/LeftNavItem.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {Link} from "react-router";
-import Animate from "rc-animate";
+import {spring, Motion} from "react-motion";
 
 class LeftNavItem extends Component{
     constructor(props) {
@@ -21,21 +21,24 @@ class LeftNavItem extends Component{
 
     render() {
         let item = this.props.item;
-        let style = {
-            height: (this.state.open ? (this.props.item.children.length + 1) * 30 : 30) + "px"
-        };
+        let rowH = 30;
+        let startH = this.state.open ? (this.props.item.children.length + 1) * rowH : rowH;;
+        let dstH = this.state.open ? rowH : (this.props.item.children.length + 1) * rowH;
+
         return (
             <li className="item-wrap">
-                <Animate>
-                    <dl className="content-wrap" style={style}>
-                        <dt onClick={this.tabNavOpen}><Link to={item.parent.link} className="first-level">{item.parent.name}</Link></dt>
-                        {
-                            item.children.map((childItem, childKey) => (
-                                <dd key={childKey}><Link to={childItem.link} className="child-item">{childItem.name}</Link></dd>
-                            ))
-                        }
-                    </dl>
-                </Animate>
+                <Motion defaultStyle={{height: startH}} style={{height: spring(dstH)}}>
+                    {
+                        value => <dl className="content-wrap" style={{height: value.height + "px"}}>
+                            <dt onClick={this.tabNavOpen}><Link to={item.parent.link} className="first-level">{item.parent.name}</Link></dt>
+                            {
+                                item.children.map((childItem, childKey) => (
+                                    <dd key={childKey}><Link to={childItem.link} className="child-item">{childItem.name}</Link></dd>
+                                ))
+                            }
+                        </dl>
+                    }
+                </Motion>
             </li>
         );
     }
